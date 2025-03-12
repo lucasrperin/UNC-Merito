f5cd527d6b73283ffa865cc700c1d34b90bb3d7b
Lucas Perin <lucasregiperin@outlook.com>
2025-03-11 21:15:07 -0300

    Ajuste pastas Model, Views e Controller

---
+++ Controllers/Prontuario.cs
@@ -0,0 +1,16 @@
+﻿namespace MeritoHealth.Controllers
+{
+    public class Prontuario
+    {
+        public int IdProntuario { get; set; }
+        public string Medico { get; set; }
+        public string Relato { get; set; }
+        public string Exames { get; set; }
+        public string Diagnostico { get; set; }
+        public DateTime DataConsulta { get; set; }
+        public string TipoConsulta { get; set; }
+        public string Medicamentos { get; set; }
+        public string Observacoes { get; set; }
+        public int IdPaciente { get; set; }
+    }
+}
